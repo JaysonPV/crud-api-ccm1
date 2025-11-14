e317baa92587ab8e46dff1343c04f42e34da1d25
Luffy <jaysonpvaldemar@gmail.com>
2025-11-14 01:35:53 +0100

    changement port et host .env

--- App/index.js
+++ App/index.js
@@ -15,7 +15,8 @@ const dbConfig = {
 	host: process.env.DB_HOST || '127.0.0.1',
 	user: process.env.DB_USER || 'root',
 	password: process.env.DB_PASSWORD || '',
-	database: process.env.DB_NAME || 'crud_app'
+	database: process.env.DB_NAME || 'crud_app',
+	port: process.env.DB_PORT || 3306
 };
 
 const LOG_DIR = '/var/logs/crud';
